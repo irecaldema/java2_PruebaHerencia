d46fda6ae28d186bfc4d33eb80adbde2f09996c5
zubiri <irecaldema14dw@ikzubirimanteo.com>
2015-01-12 13:06:43 +0000

    ejercicio de herencia

---
+++ src/EspacioPublico.java
@@ -0,0 +1,19 @@
+public class EspacioPublico{
+    //propiedades de la clase principal
+    private String nombre, direccion;
+    
+    //metodos de la clase principal
+    public void setNombre(String nombre) {
+        this.nombre=nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }    
+    
+    public void setDireccion(String direccion) {
+        this.direccion=direccion;
+    }
+    public String getDireccion() {
+        return direccion;
+    }    
+}
---
+++ src/Plaza.java
@@ -0,0 +1,13 @@
+public class Plaza extends EspacioPublico {
+    
+    //propiedades extension de la clase principal
+    private int capacidad;
+    
+    //metodos extension de la clase principal
+    public void setCapacidad(int capacidad) {
+        this.capacidad=capacidad;
+    }
+    public int getCapacidad() {
+        return capacidad;
+    }    
+}
